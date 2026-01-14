db857cebcdfb011290e23ea76ef4de0e4557c98b
anouarkharef <kharefanouar7@gmail.com>
2026-01-14 20:54:29 +0100

    Add files via upload

---
+++ food-order/partials-front/footer.php
@@ -0,0 +1,28 @@
+<!-- reseaux sociaux tebda hna-->
+    <section class="social">
+        <div class="container text-center">
+            <ul>
+                <li>
+                    <a href="#"><img src="https://img.icons8.com/fluent/50/000000/facebook-new.png"/></a>
+                </li>
+                <li>
+                    <a href="#"><img src="https://img.icons8.com/fluent/48/000000/instagram-new.png"/></a>
+                </li>
+                <li>
+                    <a href="#"><img src="https://img.icons8.com/fluent/48/000000/twitter.png"/></a>
+                </li>
+            </ul>
+        </div>
+    </section>
+    <!-- reseaux sociaux ta5las hna -->
+
+    <!-- footer yebda hna -->
+    <section class="footer">
+        <div class="container text-center">
+            <p>All rights reserved. Designed By <a href="#">Anouar Kharef </a></p>
+        </div>
+    </section>
+    <!-- footer yebda hna -->
+
+</body>
+</html>
